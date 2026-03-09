f17132aa26e0a02ab18dc24e2b1e097c85560239
Tran Huu Nguyen Son <146717584+transon999123@users.noreply.github.com>
2026-03-09 14:55:57 +0700

    Update xorimg.py

--- intro/xorimg/xorimg.py
+++ intro/xorimg/xorimg.py
@@ -1,11 +1,13 @@
+#download cv2: pip install opencv-contrib-python
+#xor 2 ảnh vô với nhau
 import cv2
 i1=cv2.imread('img1.png')
 i2=cv2.imread('img2.png')
 if i1 is not None and i2 is not None and i1.shape == i2.shape:
-    # Perform the bitwise XOR operation
+    # XOR bitwise 2 ảnh
     result_image = cv2.bitwise_xor(i1, i2)
 
-    # Display the result
+    # In ảnh
     cv2.imshow("XOR Result", result_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
